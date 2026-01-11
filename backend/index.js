de47f56a0af74d44237ea6e6c9e073cbeccb1802
Fiia Haavisto <fiiahaav@example.com>
2026-01-11 20:30:11 +0200

    Lisää frontendistä

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')  // Tarvitaan polkujen hallintaan
 const app = express()
 
 app.use(cors())
@@ -27,10 +28,14 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use(express.static('dist'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
